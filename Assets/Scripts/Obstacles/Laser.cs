9420527e9d94e1e00ed272e371ca899c8380532a
Denis Bondar <60221683+Dunno-official@users.noreply.github.com>
2020-12-11 13:50:19 +0200

    Laser
    Laser was added

--- Assets/Scripts/Obstacles/Laser.cs
+++ Assets/Scripts/Obstacles/Laser.cs
@@ -7,46 +7,68 @@ public class Laser : MonoBehaviour
 {
     [SerializeField]
     private float _laserDistance = 5;
-    private LineRenderer _laser;
 
     [SerializeField]
     private ParticleSystemRenderer[] _sourceEnergyParticles = null;
 
     [SerializeField]
-    private ParticleSystem[] _launchParticles = null;
+    private float _workTime = 3f;
 
     [SerializeField]
-    private float speedOfDistortion = 0.01f;
+    private float _coolDownTime = 2f;
+
+    //[SerializeField]
+    //private ParticleSystem[] _launchParticles = null;
+
+    private LineRenderer _laser;
+
+    private float _speedOfDistortion = 0.01f;
     private string _noiseAmountName = "Vector1_98180462";
     private string _noisePowerName  = "Vector1_CEBC9821";
 
     private Action <Material , float> _setDistortion;
-    private bool glassy = false;
+    private bool _enabled = true;
+
+    private BoxCollider2D _collider;
 
 
     private void Start()
     {
+        // просто подобрал значения
         Vector3 targetPosition = transform.position - transform.up / 2 - transform.right * 0.04f;
 
         _laser = GetComponentInChildren<LineRenderer>();
         _laser.SetPosition(0, targetPosition);
         _laser.SetPosition(1, targetPosition + transform.up * _laserDistance);
 
+        _collider = GetComponent<BoxCollider2D>();
+        _collider.size = new Vector2(_collider.size.x, _laserDistance);
+        _collider.offset = new Vector2(_collider.offset.x, 0.5f * _laserDistance - 0.5f); 
+        // Offset.y(size.y) = 0.5 * size.y - 0.5 функция зависимости смещение от размера
 
         _setDistortion = Disappear;
+
+        StartCoroutine(LaunchLaser());
     }
 
 
-    private void SetForceVector(Vector3 vector , ParticleSystem particleSystem)
-    {
-        var targetVector = particleSystem.transform.parent.position;
-        targetVector.x = particleSystem.forceOverLifetime.xMultiplier;
-    }
+    //private void SetForceVector(Vector3 vector , ParticleSystem particleSystem)
+    //{
+    //    var targetVector = particleSystem.transform.parent.position;
+    //    targetVector.x = particleSystem.forceOverLifetime.xMultiplier;
+    //}
 
 
-    public void StartCor()
+    private IEnumerator LaunchLaser()
     {
-        StartCoroutine(SwichOff());
+        while (true)
+        {
+            yield return new WaitForSeconds(_workTime);
+            yield return StartCoroutine(ToggleLaser());
+
+            yield return new WaitForSeconds(_coolDownTime);
+            yield return StartCoroutine(ToggleLaser());
+        }
     }
 
 
@@ -57,24 +79,32 @@ public class Laser : MonoBehaviour
     }
 
 
-    private IEnumerator SwichOff()
+    private IEnumerator ToggleLaser()
     {
-        StartCoroutine(MakeDistortion(glassy, (result) => { _setDistortion(_sourceEnergyParticles[0].material, result); }));
-        StartCoroutine(MakeDistortion(glassy, (result) => { _setDistortion(_sourceEnergyParticles[1].material, result); }));
+        if (!_enabled)
+        _collider.enabled = true;
 
-        yield return StartCoroutine(MakeDistortion(glassy, (result) => { _setDistortion(_laser.material, result); }));
+        _speedOfDistortion = _enabled ? 0.005f : 0.015f;
 
-        glassy = !glassy;
+        StartCoroutine(MakeDistortion((result) => { _setDistortion(_sourceEnergyParticles[0].material, result); }));
+        StartCoroutine(MakeDistortion((result) => { _setDistortion(_sourceEnergyParticles[1].material, result); }));
+        yield return StartCoroutine(MakeDistortion((result) => { _setDistortion(_laser.material, result); }));
+
+        _enabled = !_enabled;
     }
 
 
-    private IEnumerator MakeDistortion(bool glassy, Action<float> callback)
+    private IEnumerator MakeDistortion(Action<float> callback)
     {
-        float noiseDistortion = glassy ? 1 : 0.25f;
+        float noiseDistortion = _enabled ? 0.25f : 1;
 
-        while (glassy ? noiseDistortion > 0.25 : noiseDistortion < 1 )
+        while (_enabled ? noiseDistortion < 1 : noiseDistortion > 0.25)
         {
-            noiseDistortion += glassy ? -speedOfDistortion : speedOfDistortion;
+            noiseDistortion += _enabled ? _speedOfDistortion : -_speedOfDistortion;
+
+            if (noiseDistortion > 0.75f && _enabled) // когда лазер почти потух
+                _collider.enabled = false;
+
             callback(noiseDistortion);
             yield return new WaitForFixedUpdate();
         }
