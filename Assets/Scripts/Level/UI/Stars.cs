a34c4a5d21cf3c55a0f85a9f2a94bf3be9f213cc
Denis Bondar <60221683+Dunno-official@users.noreply.github.com>
2021-07-12 21:46:40 +0300

    Stars logic: start

--- Assets/Scripts/Level/UI/Stars.cs
+++ Assets/Scripts/Level/UI/Stars.cs
@@ -3,8 +3,44 @@
 
 public class Stars : MonoBehaviour
 {
+    [SerializeField] private int _gold = 2;
+    [SerializeField] private int _silver = 5;
+    [SerializeField] private SwipeHandler _swipeHandler = null;
+
+    private int _characterRotations = 0;
+
+    
     public int GetStars()
     {
-        return 3;
+        if (_characterRotations <= _gold)
+        {
+            return 3;
+        }
+
+        else if (_characterRotations > _gold && _characterRotations <= _silver)
+        {
+            return 2;
+        }
+
+        return 1;
+    }
+
+
+    private void Awake()
+    {
+        _swipeHandler.GravityChanged += OnGravityChanged;
+    }
+
+
+    private void OnDestoy()
+    {
+        _swipeHandler.GravityChanged -= OnGravityChanged;
+    }
+
+
+    private void OnGravityChanged(GravityDirection gravityDirection)
+    {
+        ++_characterRotations;
+        Debug.Log(_characterRotations);
     }
 }
