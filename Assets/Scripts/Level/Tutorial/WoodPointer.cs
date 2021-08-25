af322c62313d62608456c5c218b3f2c44c1f4399
Denis Bondar <60221683+Dunno-official@users.noreply.github.com>
2021-08-25 22:26:06 +0300

    Tutorial: final

---
+++ Assets/Scripts/Level/Tutorial/WoodPointer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using Coffee.UIEffects;
+using UnityEngine;
+
+public class WoodPointer : MonoBehaviour
+{
+    private readonly float _dissolvingSpeed = 0.5f;
+    [SerializeField] private UIDissolve _dissolveImage = null;
+    
+
+    public void ShowImage(int delayInMilliseconds)
+    {
+        StartCoroutine(ShowImageSmoothly(delayInMilliseconds));
+    }
+
+
+    private IEnumerator ShowImageSmoothly(int delayInMilliseconds)
+    {
+        yield return new WaitForSeconds(delayInMilliseconds / 1000f);
+
+        while (_dissolveImage.effectFactor >= 0)
+        {
+            _dissolveImage.effectFactor -= _dissolvingSpeed * Time.deltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
