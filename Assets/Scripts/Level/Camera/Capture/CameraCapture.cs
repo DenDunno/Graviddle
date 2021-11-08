6af165605a5705bf58baeaf158bcae56b00d0a34
Denis Bondar <60221683+Dunno-official@users.noreply.github.com>
2021-11-08 14:54:11 +0200

    Smooth falling: start

--- Assets/Scripts/Level/Camera/Capture/CameraCapture.cs
+++ Assets/Scripts/Level/Camera/Capture/CameraCapture.cs
@@ -3,10 +3,12 @@
 
 public class CameraCapture : MonoBehaviour
 {
+    private CaptureUpdate _captureUpdate;
     private CameraCapturePresenter _capturePresenter;
     private CameraBorders _cameraBorders;
     private Transform _mainCamera;
-    private float _captureSpeed = 3f;
+    private Rigidbody2D _rigidbody;
+    private readonly float _velocityToChangeUpdateMethod = 14f;
 
 
     private void Start()
@@ -14,6 +16,30 @@ public class CameraCapture : MonoBehaviour
         _capturePresenter = Camera.main.GetComponent<CameraCapturePresenter>();
         _cameraBorders = _capturePresenter.GetComponent<CameraBorders>();
         _mainCamera = _capturePresenter.transform;
+        _rigidbody = transform.GetComponent<Rigidbody2D>();
+
+        _captureUpdate = new CaptureUpdate(_mainCamera, transform, _cameraBorders);
+    }
+    
+
+    private void LateUpdate()
+    {
+        TryMoveCamera(_rigidbody.velocity.magnitude < _velocityToChangeUpdateMethod);
+    }
+
+
+    public void FixedUpdate()
+    {
+        TryMoveCamera(_rigidbody.velocity.magnitude > _velocityToChangeUpdateMethod);
+    }
+
+
+    private void TryMoveCamera(bool cameraMovementCondition)
+    {
+        if (cameraMovementCondition)
+        {
+            _captureUpdate.MoveCamera();
+        }
     }
 
 
@@ -28,26 +54,9 @@ public class CameraCapture : MonoBehaviour
     }
 
 
-    private void FixedUpdate()
-    {
-        _mainCamera.position = Vector3.Lerp(_mainCamera.position, GetNewPosition(), _captureSpeed * Time.deltaTime);
-    }
-
-
-    private Vector3 GetNewPosition()
-    {
-        Vector3 newCameraPosition = transform.position;
-        newCameraPosition.z = _mainCamera.transform.position.z;
-
-        _cameraBorders.ClampCamera(ref newCameraPosition);
-
-        return newCameraPosition;
-    }
-
-
     public void ResetCameraTransform()
     {
-        _mainCamera.position = GetNewPosition();
+        _mainCamera.position = _captureUpdate.GetNewPosition();
         _mainCamera.rotation = transform.rotation;
     }
 }
---
+++ Assets/Scripts/Level/Camera/Capture/CaptureUpdate.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+
+public class CaptureUpdate
+{
+    private readonly Transform _mainCamera;
+    private readonly Transform _target;
+    private readonly CameraBorders _cameraBorders;
+    private readonly float _captureTime = 0.3f;
+    private Vector3 _velocity;
+
+
+    public CaptureUpdate(Transform camera, Transform target, CameraBorders cameraBorders)
+    {
+        _mainCamera = camera;
+        _target = target;
+        _cameraBorders = cameraBorders;
+    }
+
+
+    public void MoveCamera()
+    {
+        _mainCamera.position = Vector3.SmoothDamp(_mainCamera.position, GetNewPosition(), ref _velocity, _captureTime);
+    }
+
+
+    public Vector3 GetNewPosition()
+    {
+        Vector3 newCameraPosition = _target.position;
+        newCameraPosition.z = _mainCamera.transform.position.z;
+
+        _cameraBorders.ClampCamera(ref newCameraPosition);
+
+        return newCameraPosition;
+    }
+}
