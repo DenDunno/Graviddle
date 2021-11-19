aac3cdb99a7515de97a02f51526811671e2faa4f
Denis Bondar <60221683+Dunno-official@users.noreply.github.com>
2021-11-19 19:08:58 +0200

    Save system: start

---
+++ Assets/Scripts/SaveSystm/LevelResultSave.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Zenject;
+
+public class LevelResultSave : MonoBehaviour
+{
+    [Inject] private readonly CharacterStatesPresenter _characterStatesPresenter = null;
+    [SerializeField] private Reward _reward = null;
+
+
+    public void OnEnable()
+    {
+        _characterStatesPresenter.WinState.CharacterWon += SaveLevelResult;
+    }
+
+
+    public void OnDisable()
+    {
+        _characterStatesPresenter.WinState.CharacterWon -= SaveLevelResult;
+    }
+
+
+    private void SaveLevelResult()
+    {
+        string savePath = Application.persistentDataPath + "Save.json";
+        string json = File.ReadAllText(savePath);
+
+        var save = JsonConvert.DeserializeObject<Dictionary<int, int>>(json);
+
+        save[SceneManager.GetActiveScene().buildIndex] = _reward.GetStars();
+
+        json = JsonConvert.SerializeObject(save);
+
+        File.WriteAllText(savePath , json);
+    }
+}
---
+++ Assets/Scripts/SaveSystm/LevelResultsLoading.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LevelResultsLoading : MonoBehaviour
+{
+    [SerializeField] private GridLayoutGroup _levels = null;
+    private Button[] _allButtons;
+
+
+    private void Start()
+    {
+        string savePath = Application.persistentDataPath + "Save.json";
+
+        if (TryCreateDirectory(savePath) == false) // save exists
+        {
+            Dictionary<int,int> saves = LoadSaves(savePath);
+
+        }
+    }
+
+
+    private bool TryCreateDirectory(string savePath)
+    {
+        bool noDirectory = false;
+
+        if (File.Exists(savePath) == false)
+        {
+            File.Create(savePath);
+            noDirectory = true;
+        }
+
+        return noDirectory;
+    }
+
+
+    private Dictionary<int, int> LoadSaves(string savePath)
+    {
+        string json = File.ReadAllText(savePath);
+
+        return JsonConvert.DeserializeObject<Dictionary<int, int>>(json);
+    }
+}
