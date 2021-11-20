488a2315a6008648e0af4cc25caaa104f602d535
Denis Bondar <60221683+Dunno-official@users.noreply.github.com>
2021-11-20 16:52:11 +0200

    ParallaxLayer

---
+++ Assets/Scripts/Menu/ParallaxLayer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(RectTransform))]
+public class ParallaxLayer : MonoBehaviour
+{
+    [SerializeField] private Part _parallaxPart = Part.Left;
+    [SerializeField] private float _speed = 0;
+    private RectTransform _transform;
+    private float _leftBorder;
+    private float _rightBorder;
+
+    private enum Part
+    {
+        Left,
+        Centre,
+        Right
+    }
+
+    private void Start()
+    {
+        _transform = GetComponent<RectTransform>();
+
+        _leftBorder = -_transform.rect.width;
+        _rightBorder = _transform.rect.width;
+
+        if (_parallaxPart != Part.Centre)
+        {
+            float xPosition = (_parallaxPart == Part.Left ? _leftBorder : _rightBorder);
+
+            _transform.anchoredPosition = new Vector2(xPosition , _transform.anchoredPosition.y);
+        }
+    }
+    
+
+    public void Update()
+    {
+        _transform.anchoredPosition += Vector2.left * _speed * Time.deltaTime;
+
+        if (_transform.anchoredPosition.x <= _leftBorder)
+        {
+            _transform.anchoredPosition = new Vector2(_rightBorder, _transform.anchoredPosition.y);
+        }
+    }
+}
