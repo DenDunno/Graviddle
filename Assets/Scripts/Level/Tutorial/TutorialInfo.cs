f23cc55cd7f0d4b12758d8caa0bf4e5ae5c28d47
Denis Bondar <60221683+Dunno-official@users.noreply.github.com>
2021-11-27 20:53:53 +0200

    Tutorial: movement buttons

---
+++ Assets/Scripts/Level/Tutorial/TutorialInfo.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Zenject;
+
+
+public class TutorialInfo : MonoBehaviour
+{
+    [Inject] private readonly CharacterStatesPresenter _characterStatesPresenter = null;
+
+
+    private void OnEnable()
+    {
+        _characterStatesPresenter.WinState.CharacterWon += OnCharacterWon;
+    }
+
+
+    private void OnDisable()
+    {
+        _characterStatesPresenter.WinState.CharacterWon -= OnCharacterWon;
+    }
+
+
+    private void OnCharacterWon()
+    {
+        gameObject.SetActive(false);
+    }
+}
