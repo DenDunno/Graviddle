c52da1ec19fa56750fa1e9a3a10ddaf521f1070a
Denis Bondar <60221683+Dunno-official@users.noreply.github.com>
2022-02-01 22:31:31 +0200

    small refactoring

--- Assets/Scripts/Level/UI/Switchers/EditorUISwitcher.cs
+++ Assets/Scripts/Level/UI/Switchers/EditorUISwitcher.cs
@@ -1,17 +1,15 @@
 using UnityEngine;
 
 
-public class EditorGameSwitcher : MonoBehaviour
+public class EditorUISwitcher : MonoBehaviour
 {
     [SerializeField] private UIStatesSwitcher _uiStatesSwitcher;
-    [SerializeField] private CharacterInputButton[] _inputButtons;
     
 
     private void Start()
     {
         #if  UNITY_EDITOR
         _uiStatesSwitcher.gameObject.SetActive(true);
-        _inputButtons.ForEach(inputButton => inputButton.gameObject.SetActive(false));
         #endif
     }
 }
