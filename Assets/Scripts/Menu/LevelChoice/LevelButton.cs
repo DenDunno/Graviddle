356036c1da1c007392276f23a6e5c6cbd021a9e3
Denis Bondar <60221683+DenDunno@users.noreply.github.com>
2022-02-07 18:02:20 +0200

    Non stars, restart button

--- Assets/Scripts/Menu/LevelChoice/LevelButton.cs
+++ Assets/Scripts/Menu/LevelChoice/LevelButton.cs
@@ -6,14 +6,16 @@ using UnityEngine.UI;
 public class LevelButton : MonoBehaviour
 {
     [SerializeField] private Image[] _stars;
+    [SerializeField] private Image[] _nonStars;
     [SerializeField] private MPImage _lockImage;
 
 
     public void SetStars(int stars)
     {
-        for (int i = 0; i < stars; i++)
+        for (var i = 0; i < stars; i++)
         {
             _stars[i].gameObject.SetActive(true);
+            _nonStars[i].gameObject.SetActive(false);
         }
     }
 
