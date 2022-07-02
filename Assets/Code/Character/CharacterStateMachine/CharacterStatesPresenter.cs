577a7db785596dfe4c86bed7019997c617f9c80e
DenDunno <60221683+DenDunno@users.noreply.github.com>
2022-07-02 11:23:51 +0300

    .

--- Assets/Code/Character/CharacterStateMachine/CharacterStatesPresenter.cs
+++ Assets/Code/Character/CharacterStateMachine/CharacterStatesPresenter.cs
@@ -15,7 +15,7 @@ public class CharacterStatesPresenter
 
     public CharacterStatesPresenter(Animator character, CharacterMoveDirection characterMoveDirection)
     {
-        FallState = new FallState(character, characterMoveDirection);
+        FallState = new FallState(character);
         DieState = new DieState(character);
         WinState = new WinState(character);
         IdleState = new IdleState(character);
--- Assets/Code/Character/CharacterStateMachine/States/FallState.cs
+++ Assets/Code/Character/CharacterStateMachine/States/FallState.cs
@@ -4,28 +4,16 @@ using UnityEngine;
 
 public class FallState : CharacterState
 {
-    private readonly Rigidbody2D _rigidbody;
-    private readonly CharacterMoveDirection _characterMoveDirection;
-    private const float _movementSpeed = 8f;
     public event Action CharacterFalling;
 
 
-    public FallState(Animator character, CharacterMoveDirection characterMoveDirection) : base(character, AnimationsName.Fall)
+    public FallState(Animator character) : base(character, AnimationsName.Fall)
     {
-        _rigidbody = character.GetComponent<Rigidbody2D>();
-        _characterMoveDirection = characterMoveDirection;
     }
 
     
     protected override void OnEnterState()
-    { 
-        _rigidbody.velocity = Vector2.zero;;
-        CharacterFalling?.Invoke();
-    }
-    
-
-    public override void Update()
     {
-        _rigidbody.velocity += _characterMoveDirection.Direction * (_movementSpeed * Time.deltaTime);
+        CharacterFalling?.Invoke();
     }
 }
